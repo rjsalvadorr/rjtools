649d6cef0d6ad2866d2fe44a05e7b491510b8e7f
Randolph Salvador <randolph.salvador@intelex.com>
2018-02-05 10:48:14 -0500

    New version of rjmdpdf is even better!

---
+++ rjnode/rjmdpdf.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Check if NodeJS is usable.
+if node --version | grep -q 'not found'; then
+  echo "[ Node JS is required, but not found! ]"
+  exit 1
+fi
+
+# Check if markdown-pdf is usable.
+if mdpdf --version | grep -q 'not found'; then
+  echo "[ The mdpdf package isn't found. Attempting to install... ]"
+  npm install -g mdpdf
+fi
+
+# Check if a filename has been given.
+if [ -z "$1" ]
+  then
+    echo "[ No filename given! ]"
+	exit 1
+fi
+
+echo "[ Converting" $1 "to a PDF ]"
+
+mdpdf $1 --style ./rjmdpdf.css --border 0.5in
+
+echo "[ Conversion done ]"
+exit 0
