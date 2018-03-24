85b0198206214e9b18a21795a1b2be636535da95
RJ Salvador <randolph.salvador@gmail.com>
2018-03-24 15:36:01 -0400

    Added timestamp generator module

---
+++ node/timestampgenerator.js
@@ -0,0 +1,51 @@
+// For generating timestamps
+
+var DEBUG_MODE = false;
+
+class TimestampGenerator {
+    constructor() {
+        this.moment = require('moment');
+    }
+
+    getSimpleDate() {
+        var date = this.moment().format('YYYY-MMM-DD');
+        return date.toLowerCase();
+    }
+
+    getYearDay() {
+        var year = this.moment().format('YYYY');
+        var day = this.moment().format('DDDD');
+        return year + '-day-' + day;
+    }
+
+    getYearWeek() {
+        var year = this.moment().format('YYYY');
+        var week = this.moment().format('ww');
+        return year + '-week-' + week;
+    }
+
+    getTimestamp() {
+        return this.moment().unix();
+    }
+
+    getTimestampMilliseconds() {
+        return this.moment().valueOf();
+    }
+
+    getTimestampHex() {
+        return this.getTimestamp().toString(16);
+    }
+
+    getTimestampMillisecondsHex() {
+        return this.getTimestampMilliseconds().toString(16);
+    }
+};
+
+module.exports = TimestampGenerator;
+
+if(DEBUG_MODE) {
+    var timestampGenerator = new TimestampGenerator();
+    console.log('getSimpleDate()', timestampGenerator.getSimpleDate())
+    console.log('getYearDay()', timestampGenerator.getYearDay())
+    console.log('getYearWeek()', timestampGenerator.getYearWeek())
+}
