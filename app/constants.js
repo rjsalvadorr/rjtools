25e12515216dbf942f0a06c5e3b4d69788d96f70
Randolph Salvador <randolph.salvador@intelex.com>
2018-04-03 11:51:59 -0400

    Added constants file for app

---
+++ app/constants.js
@@ -0,0 +1,11 @@
+
+class Constants {
+  constructor() {
+    this.ROOT_DIRECTORY = '';
+    this.DATA_DIRECTORY = '';
+    this.TEMPLATES_DIRECTORY = '';
+	this.SETTINGS_DIRECTORY = '';
+  }
+}
+
+module.exports = new Constants();
